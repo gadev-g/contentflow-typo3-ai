cf51a52cce5a474e2f5fe7c611760f25aad3e08b
ContentFlow AI <development@contentflow-ai.com>
2026-07-29 15:29:36 +0200

    fix(migration): resolve bundled eID endpoint locally

--- LegacySourceConnector/ext_localconf.php
+++ LegacySourceConnector/ext_localconf.php
@@ -4,9 +4,7 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$exportEndpoint = \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName(
-    'EXT:contentflow_legacy_source/Classes/Eid/ExportEndpoint.php'
-);
+$exportEndpoint = __DIR__.'/Classes/Eid/ExportEndpoint.php';
 
 if (is_string($exportEndpoint) && '' !== $exportEndpoint && is_file($exportEndpoint)) {
     $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['contentflow_migration_export'] = $exportEndpoint;

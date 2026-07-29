2091af012e5cc147a98ed9d37d3c51e85bc85a50
ContentFlow AI <development@contentflow-ai.com>
2026-07-29 15:26:04 +0200

    fix(migration): guard legacy eID registration

--- LegacySourceConnector/ext_localconf.php
+++ LegacySourceConnector/ext_localconf.php
@@ -4,6 +4,12 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['contentflow_migration_export']
-    = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('contentflow_legacy_source')
-    .'Classes/Eid/ExportEndpoint.php';
+$exportEndpoint = \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName(
+    'EXT:contentflow_legacy_source/Classes/Eid/ExportEndpoint.php'
+);
+
+if (is_string($exportEndpoint) && '' !== $exportEndpoint && is_file($exportEndpoint)) {
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['contentflow_migration_export'] = $exportEndpoint;
+}
+
+unset($exportEndpoint);
